12483c4ec776cb279fcb1769954e9ea48e072ba8
Gary Ewan Park <gep13@gep13.co.uk>
2018-05-01 13:14:33 +0100

    (GH-11) Added -y to all Chocolatey calls
    - Used the same style as per this commit: https://github.com/Microsoft/windows-dev-box-setup-scripts/commit/4ab2d140ab3191e83a37d49eb732cd5eeee330d1

--- dev_app.ps1
+++ dev_app.ps1
@@ -13,13 +13,13 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
 
 #--- Windows Subsystems/Features ---
-choco install Microsoft-Hyper-V-All -source windowsFeatures
-choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+choco install -y Microsoft-Hyper-V-All -source windowsFeatures
+choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
 
 #--- Tools ---
-choco install visualstudio2017community -y  # See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
-choco install sysinternals -y
-choco install docker-for-windows
+choco install -y visualstudio2017community  # See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
+choco install -y sysinternals
+choco install -y docker-for-windows
 
 Enable-UAC
 Enable-MicrosoftUpdate
