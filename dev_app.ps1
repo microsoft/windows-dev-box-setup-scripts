40eaa2c60aa13ef5d50d8c6ef2fe2b47c1592957
Clint Rutkas <crutkas@microsoft.com>
2018-06-10 00:25:28 -0700

    Initial attempt at segmenting stuff off

--- dev_app.ps1
+++ dev_app.ps1
@@ -10,54 +10,33 @@ Disable-UAC
 #choco install -y sysinternals
 #choco install -y docker-for-windows
 
-#File Explorer remote call
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/crutkas/windows-dev-box-setup-scripts/TestRemotePs/helper_SetFileExplorer.ps1'))
+$user = "crutkas";
+$baseBranch = "master";
+$finalBaseHelperUri = "https://raw.githubusercontent.com/$user/windows-dev-box-setup-scripts/$baseBranch/scripts";
 
+#Setting up Windows
+executeScript("FileExplorerSettings.ps1");
+executeScript("RemoveDefaultApps.ps1");
 
+#--- Tools ---
 #--- VS 2017 uwp and azure workloads + git tools ---
 # See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
 # https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community 
 # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
 choco install visualstudio2017community --package-parameters "--add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Component.Git"
-RefreshEnv
+RefreshEnv #refreshing env due to Git install
 
+#--- UWP Workload and installing Windows Template Studio
 choco install visualstudio2017-workload-universal
-
-#--- grabbing latest UWP Samples off Github ---
-RefreshEnv
-cd $env:USERPROFILE\desktop
-mkdir UwpSamples
-cd UwpSamples
-git clone https://github.com/Microsoft/Windows-universal-samples/
-
-# installing Windows Template Studio VSIX
-#choco was claiming dev 15 can't do this anymore for vsix... odd hasn't been fixed yet
-#Install-ChocolateyVsixPackage -PackageName "Windows Template Studio" -VsixUrl $wtsFileUrl.source
-
-$requestUri = 'https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery'
-$requestBody = '{"flags":"262","filters":[{"criteria":[{"filterType":"10","value":"windows template studio"}],"sortBy":"0","sortOrder":"2","pageSize":"25","pageNumber":"1"}]}'
-$requestHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$requestHeaders.Add('Accept','application/json; api-version=3.2-preview.1')
-$requestHeaders.Add('Content-Type','application/json; charset=utf-8')
-
-$results = Invoke-WebRequest -Uri $requestUri -Method POST -Headers $requestHeaders -Body $requestBody -UseBasicParsing
-
-$jsonResults = $results.Content | ConvertFrom-Json
-$wtsResults = $jsonResults.results[0].extensions | where {$_.extensionName -eq "WindowsTemplateStudio"} 
-$wtsFileUrl = $wtsResults.versions[0].files | where {$_.assetType -eq "Microsoft.Templates.2017.vsix"}
-
-$wtsVsix = [System.IO.Path]::GetFileName($wtsFileUrl.source)
-$wtsFullPath = [System.IO.Path]::Combine((Resolve-Path .\).Path, $wtsVsix);
-Invoke-WebRequest -Uri $wtsFileUrl.source -OutFile $wtsVsix
-$vsixInstallerFile = Get-Childitem -Include vsixinstaller.exe -Recurse -Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\"
-$wtsArgList = $wtsFullPath + " /q"
-
-$vsixInstallerResult = Start-Process -FilePath $vsixInstallerFile.FullName -ArgumentList $wtsArgList -Wait -PassThru;
-
-Remove-Item $wtsVsix
-
+executeScript("WindowsTemplateStudio.ps1");
+executeScript("GetUwpSamplesOffGithub.ps1");
 
 #--- reenabling critial items ---
 Enable-UAC
 Enable-MicrosoftUpdate
 Install-WindowsUpdate -acceptEula
+
+function executeScript {
+	Param ([string]$script)
+	iex ((new-object net.webclient).DownloadString("$finalBaseHelperUri/$script"))
+}
---
+++ scripts/GetUwpSamplesOffGithub.ps1
@@ -0,0 +1,5 @@
+RefreshEnv
+cd $env:USERPROFILE\desktop
+mkdir UwpSamples
+cd UwpSamples
+git clone https://github.com/Microsoft/Windows-universal-samples/
