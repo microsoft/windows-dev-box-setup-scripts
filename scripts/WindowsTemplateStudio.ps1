bc1aae2f0272f8ce406cd90e7e9e191f12e39f6a
Clint Rutkas <clint@rutkas.com>
2018-06-13 08:07:09 -0700

    Update WindowsTemplateStudio.ps1

--- scripts/WindowsTemplateStudio.ps1
+++ scripts/WindowsTemplateStudio.ps1
@@ -15,11 +15,11 @@ $wtsVsix = [System.IO.Path]::GetFileName($wtsFileUrl.source)
 $desktopPath = Resolve-Path $env:USERPROFILE\desktop
 $wtsFullPath = [System.IO.Path]::Combine($desktopPath, $wtsVsix);
 
-Invoke-WebRequest -Uri $wtsFileUrl.source -OutFile `"$wtsVsix`"
+Invoke-WebRequest -Uri $wtsFileUrl.source -OutFile $wtsFullPath
 
 $vsixInstallerFile = Get-Childitem -Include vsixinstaller.exe -Recurse -Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\"
 $wtsArgList = $wtsFullPath + " /q"
 
 $vsixInstallerResult = Start-Process -FilePath $vsixInstallerFile.FullName -ArgumentList `"$wtsArgList`" -Wait -PassThru;
 
-Remove-Item $wtsVsix
+Remove-Item $wtsFullPath
