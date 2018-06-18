2b1e800dbb5e4561c518f547cae0160adfded5b1
molant <amolleda@gmail.com>
2018-06-18 12:17:48 -0700

    New: Install VS Code debugger extensions
    Install automatically VS Code debugger extensions for Chrome and Edge
    
    - - - - - - - - - - - - - - -
    
    Ref: #46

--- dev_web_nodejs.ps1
+++ dev_web_nodejs.ps1
@@ -15,6 +15,10 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 
 #--- Tools ---
 choco install -y vscode
+refreshenv
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension msjsdiag.debugger-for-edge
+
 choco install -y git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"'
 choco install -y 7zip.install
 
