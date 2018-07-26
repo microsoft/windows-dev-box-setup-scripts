690c67c9e32869ce61d2367434809094991f715a
yodurr <yodurr@microsoft.com>
2018-07-26 13:36:04 -0700

    adding dev mode back in

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -1,5 +1,4 @@
 
-#TODO: The dev mode command is failing so commenting this out until we fix it
 
 #--- Enable developer mode on the system ---
-# Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
