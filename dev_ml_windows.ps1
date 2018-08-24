4b8dfb538e2639eabdc1f3a711ccb2aa2403e22f
Yosef Durr <yodurr@microsoft.com>
2018-08-24 15:43:14 -0700

    merging recent ML changes by craig

--- dev_ml_windows.ps1
+++ dev_ml_windows.ps1
@@ -20,7 +20,8 @@ function executeScript {
 executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
-executeScript "CommonDevTools.ps1";
+executeScript "GetMLIDEAndTooling.ps1";
+executeScript "PythonMLTools.ps1";
 
 Enable-UAC
 Enable-MicrosoftUpdate
