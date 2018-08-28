b8fd2780d98cf46f95573b0c8c90f5a744a9ab63
Yosef Durr <yodurr@microsoft.com>
2018-08-28 13:42:27 -0700

    fixing sample path

--- scripts/GetMLPythonSamplesOffGithub.ps1
+++ scripts/GetMLPythonSamplesOffGithub.ps1
@@ -3,4 +3,4 @@ write-host "Downloading Python ML samples to your desktop ..."
 
 Update-SessionEnvironment
 cd $env:USERPROFILE\desktop
-git clone https://github.com/Microsoft/Dev-Advocacy-Samples/tree/master/python-mlclassification
+git clone https://github.com/Microsoft/Dev-Advocacy-Samples
