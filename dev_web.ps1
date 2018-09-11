295a29397e23ce13b271cba55ba3d6aea1a34fb6
Yosef Durr <yodurr@microsoft.com>
2018-09-11 15:45:52 -0700

    dev_web was missing WSL

--- dev_web.ps1
+++ dev_web.ps1
@@ -28,6 +28,7 @@ executeScript "CommonDevTools.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "HyperV.ps1";
 executeScript "Docker.ps1";
+executeScript "WSL.ps1";
 executeScript "Browsers.ps1";
 
 #--- Tools ---
