ef4452b51a6326bb9b015bb0f0a2f43e9459f510
Yosef Durr <yodurr@microsoft.com>
2018-10-03 00:23:48 -0700

    personalizing demo script

--- demos/chocolateyfest2018.ps1
+++ demos/chocolateyfest2018.ps1
@@ -26,18 +26,18 @@ executeScript "FileExplorerSettings.ps1";
 executeScript "SystemConfiguration.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
-executeScript "HyperV.ps1";
-executeScript "WSL.ps1";
-
-RefreshEnv
-
-executeScript "Docker.ps1";
 executeScript "Browsers.ps1";
 
-choco install azure-cli 
+executeScript "HyperV.ps1";
+RefreshEnv
+executeScript "WSL.ps1";
+RefreshEnv
+executeScript "Docker.ps1";
+
+choco install azure-cli
 choco install azurepowershell
 choco install microsoftazurestorageexplorer
-choco install terraform 
+choco install terraform
 
 # Install tools in WSL instance
 write-host "Installing tools inside the WSL distro..."
@@ -45,6 +45,22 @@ Ubuntu1804 run apt install ansible -y
 Ubuntu1804 run apt install nodejs -y
 
 # personalize
+choco install microsoft-teams
+choco install office365business
+choco install spotify
+
+# checkout recent projects
+mkdir C:\github
+cd C:\github
+git.exe clone https://github.com/microsoft/windows-dev-box-setup-scripts
+git.exe clone https://github.com/microsoft/winappdriver
+git.exe clone https://github.com/microsoft/wsl
+
+# set desktop wallpaper
+Invoke-WebRequest -Uri 'http://chocolateyfest.com/wp-content/uploads/2018/05/img-bg-front-page-header-NO_logo-opt.jpg' -Method Get -ContentType image/jpeg -OutFile 'C:\ghtest\chocofest.jpg'
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value 'C:\ghtest\chocofest.jpg'
+    rundll32.exe user32.dll, UpdatePerUserSystemParameters
+
 
 Enable-UAC
 Enable-MicrosoftUpdate
