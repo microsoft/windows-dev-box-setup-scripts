fafd867f4288e61ac0de34cf5155a2d6230bb307
DivisionSt <forrest.humphrey@gmail.com>
2019-01-25 08:20:01 -0600

    added a weblaunch-friendly script for a visual studio enterprise dev

---
+++ dev_dotnet_enterprise.ps1
@@ -0,0 +1,43 @@
+# Description: Boxstarter Script
+# Author: Microsoft
+# Common settings for web dev
+
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Setting up Windows ---
+executeScript "FileExplorerSettings.ps1";
+executeScript "SystemConfiguration.ps1";
+executeScript "CommonDevTools.ps1";
+executeScript "VSEnterpriseDev.ps1";
+executeScript "CommandLine.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "WSL.ps1";
+executeScript "Browsers.ps1";
+
+#--- Tools ---
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension msjsdiag.debugger-for-edge
+
+#--- Microsoft WebDriver ---
+choco install -y microsoftwebdriver
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
---
+++ scripts/VSEnterpriseDev.ps1
@@ -0,0 +1,5 @@
+
+
+choco install -y visualstudio2017enterprise
+choco install -y visualstudio2017-workload-netweb
+choco install -y visualstudio2017-workload-azure
