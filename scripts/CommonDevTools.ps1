2bf7cfa186820ac2ec19798f63add79fe8f88e53
Matt Newport <matt@mattnewport.com>
2021-02-16 14:10:37 -0800

    Add explicit cache location to CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,10 @@
+# work around a boxstarter bug creating nested temp dirs: https://github.com/chocolatey/boxstarter/issues/442
+$boxstarterTempDir = 'C:\temp'
+mkdir $boxstarterTempDir -Force
 
 # tools we expect devs across many scenarios will want
-choco install -y vscode
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python
-choco install -y 7zip.install
-choco install -y sysinternals
+choco install --cacheLocation=$boxStarterTempDir -y vscode
+choco install --cacheLocation=$boxStarterTempDir -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install --cacheLocation=$boxStarterTempDir -y python
+choco install --cacheLocation=$boxStarterTempDir -y 7zip.install
+choco install --cacheLocation=$boxStarterTempDir -y sysinternals
