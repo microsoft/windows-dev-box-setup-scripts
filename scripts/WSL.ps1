f72bef7559a0ca56af56748f40c9ba58e8b44eb3
Nick <nick@kousu.ca>
2021-03-21 01:58:03 -0400

    Deploy WSL with a regular user account
    This matches the result you get when you deploy it interactively. The only difference is we set the password for you.

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -4,12 +4,52 @@ choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
 # TODO: Move this to choco install once --root is included in that package
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
 Add-AppxPackage -Path ~/Ubuntu.appx
-# run the distro once and have it install locally with root user, unset password
+# run the distro once and have it install locally. The default account is "ubuntu:ubuntu".
 
 RefreshEnv
-Ubuntu1804 install --root
-Ubuntu1804 run apt update
-Ubuntu1804 run apt upgrade -y
+
+$distro = "ubuntu1804"
+$username = "ubuntu"
+$password = "ubuntu"
+
+& $distro install --root
+if ($LASTEXITCODE -ne 0) { throw "Could not install distro." }
+
+# the only non-interactive way to set up a WSL distro is the --root flag
+#    https://github.com/microsoft/WSL/issues/3369
+# but it has the side effect of making all `wsl` calls run as root,
+# so users still need to manually intervene to set up a regular account.
+# Workaround this by installing with --root but then replicating,
+# noninteractively, what happens in the WSL DistroLauncher:
+#   https://github.com/microsoft/WSL-DistroLauncher/blob/2ed9a9335fc89a688a5150c95eff4fbdbc830f25/DistroLauncher/DistributionInfo.cpp#L8-L33
+& $distro run useradd -m "$username"
+if ($LASTEXITCODE -ne 0) { throw }
+& $distro run sh -c 'echo "${username}:${password}" | chpasswd' # wrapped in sh -c to get the pipe to work
+if ($LASTEXITCODE -ne 0) { throw }
+& $distro run chsh -s /bin/bash "$username"
+if ($LASTEXITCODE -ne 0) { throw }
+& $distro run usermod -aG adm,cdrom,sudo,dip,plugdev "$username"
+if ($LASTEXITCODE -ne 0) { throw }
+
+
+# apt install -y isn't enough to be truly noninteractive
+$env:DEBIAN_FRONTEND = "noninteractive"
+$env:WSLENV += ":DEBIAN_FRONTEND"
+
+# update software
+& $distro run apt-get update
+if ($LASTEXITCODE -ne 0) { throw }
+& $distro run apt-get full-upgrade -y
+if ($LASTEXITCODE -ne 0) { throw }
+& $distro run apt-get autoremove -y
+if ($LASTEXITCODE -ne 0) { throw }
+& $distro run apt-get autoclean
+if ($LASTEXITCODE -ne 0) { throw }
+wsl --terminate "Ubuntu-18.04"  # instead of 'reboot'
+if ($LASTEXITCODE -ne 0) { throw }
+
+& $distro config --default-user "$username"
+if ($LASTEXITCODE -ne 0) { throw }
 
 <#
 NOTE: Other distros can be scripted the same way for example:
