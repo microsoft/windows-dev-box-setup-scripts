8dedbd7a9c900f4d3aed6cc6022d8933237583ef
AndyAdelman <aadelman@gmail.com>
2023-12-24 11:36:20 -0700

    Update BasicApplications.ps1
    Some more common apps

--- scripts/BasicApplications.ps1
+++ scripts/BasicApplications.ps1
@@ -1,14 +1,50 @@
 
 # Common
-choco install -y 7zip.install
-choco install notepadplusplus -y
-choco install sumatrapdf.install
+choco install -y 7zip
+choco install -y notepadplusplus
+choco install -y sumatrapdf
 
 # Browsers
-choco install googlechrome -y
-choco install firefox -y
+choco install -y googlechrome
+choco install -y firefox
+choco install -y tor-browser
 
 # Media
-choco install vlc -y
-choco install audacity -y
-choco install imageglass -y
+choco install -y vlc
+choco install -y audacity
+choco install -y imageglass
+choco install -y qbittorrent
+
+
+# Management
+choco install -y treesizefree
+choco install -y crystaldiskinfo
+
+# Research sysinternals more
+choco install -y sysinternals
+
+# Networking
+choco install -y nordvpn
+
+# Dev
+choco install -y git
+choco install -y microsoft-windows-terminal
+choco install -y putty
+# For managing Linux and MacOS systems
+choco install -y powershell-core
+
+# Maybe
+# choco install -y autohotkey
+# choco install -y handbrake
+# choco install -y makemkv
+# choco install -y imgburn
+# I normally just use the browser extension
+# choco install -y bitwarden
+
+# Not found
+# gitbash
+# magic jellybean keyfinder
+
+
+
+
