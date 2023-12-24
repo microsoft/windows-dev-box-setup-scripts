80a10077cc20d4578dd0d1bed436f5b48a0ccc3a
AndyAdelman <aadelman@gmail.com>
2023-12-24 10:40:29 -0700

    Update BasicApplications.ps1
    Some core applications

--- scripts/BasicApplications.ps1
+++ scripts/BasicApplications.ps1
@@ -1,1 +1,14 @@
 
+# Common
+choco install -y 7zip.install
+choco install notepadplusplus -y
+choco install sumatrapdf.install
+
+# Browsers
+choco install googlechrome -y
+choco install firefox -y
+
+# Media
+choco install vlc -y
+choco install audacity -y
+choco install imageglass -y
