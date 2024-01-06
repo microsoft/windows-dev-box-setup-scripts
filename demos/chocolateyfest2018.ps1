c49dcef451040d89407673d13409176786969f0f
AndyAdelman <aadelman@gmail.com>
2024-01-06 16:11:09 -0700

    Update Boxstarter Settings & UAC

--- demos/chocolateyfest2018.ps1
+++ demos/chocolateyfest2018.ps1
@@ -1,8 +1,17 @@
-l# Description: Boxstarter Script
+# Description: Boxstarter Script: 1.0.0.2
 # Author: Microsoft
 # chocolatey fest demo
 
+#--- Boxstarter options ---
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+#--- Basic setup ---
+Update-ExecutionPolicy -Policy RemoteSigned
 Disable-UAC
+Enable-RemoteDesktop
+choco feature enable -n=allowGlobalConfirmation
 $ConfirmPreference = "None" #ensure installing powershell modules don't prompt on needed dependencies
 
 # Get the base URI path from the ScriptToCall value
@@ -24,18 +33,18 @@ function executeScript {
 }
 
 #--- Setting up Windows ---
+executeScript "PrivacySettings.ps1";
+executeScript "SecuritySettings.ps1";
 executeScript "GeneralSettings.ps1";
 executeScript "LockScreenSettings.ps1";
 executeScript "StartMenuSettings.ps1";
 executeScript "TaskbarSettings.ps1";
 executeScript "FileExplorerSettings.ps1";
-executeScript "SecuritySettings.ps1";
-executeScript "PrivacySettings.ps1";
 executeScript "WiFiSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 RefreshEnv
 executeScript "BasicApplications.ps1";
-
+RefreshEnv
 
 ## set desktop wallpaper
 #Invoke-WebRequest -Uri 'http://chocolateyfest.com/wp-content/uploads/2018/05/img-bg-front-page-header-NO_logo-opt.jpg' -Method Get -ContentType image/jpeg -OutFile 'C:\github\chocofest.jpg'
