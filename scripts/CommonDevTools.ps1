d55f8a310ae0ffa3972c26ed827478a3b57fbaba
AndyAdelman <aadelman@gmail.com>
2024-01-06 16:16:08 -0700

    Delete scripts/CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++
@@ -1,7 +0,0 @@
-
-# tools we expect devs across many scenarios will want
-choco install -y vscode
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python
-choco install -y 7zip.install
-choco install -y sysinternals
